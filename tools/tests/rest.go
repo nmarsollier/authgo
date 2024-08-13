cd5f541eee1f9ecf339f76c9d5e9674625650ff6
Nestor Marsollier <nmarsollier@gmail.com>
2024-08-13 10:12:25 -0300

    repackage

--- tools/tests/rest.go
+++ tools/tests/rest.go
@@ -1,6 +1,7 @@
 package tests
 
 import (
+	"bytes"
 	"encoding/json"
 	"net/http"
 	"net/http/httptest"
@@ -8,6 +9,28 @@ import (
 
 	"github.com/stretchr/testify/assert"
 )
+
+// Requests Test functions
+
+func TestGetRequest(url string, tokenString string) (*http.Request, *httptest.ResponseRecorder) {
+	req, _ := http.NewRequest("GET", url, bytes.NewBuffer([]byte{}))
+	if len(tokenString) > 0 {
+		req.Header.Add("Authorization", "bearer "+tokenString)
+	}
+	w := httptest.NewRecorder()
+	return req, w
+}
+
+func TestPostRequest(url string, body interface{}, tokenString string) (*http.Request, *httptest.ResponseRecorder) {
+	jsonBody, _ := json.Marshal(body)
+
+	req, _ := http.NewRequest("POST", url, bytes.NewBuffer(jsonBody))
+	if len(tokenString) > 0 {
+		req.Header.Add("Authorization", "bearer "+tokenString)
+	}
+	w := httptest.NewRecorder()
+	return req, w
+}
 
 // Assertion Functions
 func AssertUnauthorized(t *testing.T, w *httptest.ResponseRecorder) {
