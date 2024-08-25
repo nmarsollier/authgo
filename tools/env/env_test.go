905f75fc230747ce12e896454072ae4b3194d1aa
Nestor Marsollier <nmarsollier@gmail.com>
2024-08-25 10:30:54 -0300

    Update env_test.go

--- tools/env/env_test.go
+++ tools/env/env_test.go
@@ -7,10 +7,22 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+
+func TestDefault(t *testing.T) {
+	config := load()
+
+	// Assert the values
+	assert.Equal(t, "amqp://localhost", config.RabbitURL)
+	assert.Equal(t, "mongodb://localhost:27017", config.MongoURL)
+	assert.Equal(t, "localhost:24224", config.FluentUrl)
+	assert.Equal(t, 3000, config.Port)
+	assert.Equal(t, "ecb6d3479ac3823f1da7f314d871989b", config.JWTSecret)
+}
+
 func TestLoad(t *testing.T) {
 	// Set environment variables
 	os.Setenv("RABBIT_URL", "amqp://rabbitmq:5672")
-	os.Setenv("MONGO_URL", "mongodb://localhost:27017")
+	os.Setenv("MONGO_URL", "mongodb://mongourl:27017")
 	os.Setenv("FLUENT_URL", "fluentd:24224")
 	os.Setenv("PORT", "8080")
 	os.Setenv("JWT_SECRET", "mysecret")
@@ -20,7 +32,7 @@ func TestLoad(t *testing.T) {
 
 	// Assert the values
 	assert.Equal(t, "amqp://rabbitmq:5672", config.RabbitURL)
-	assert.Equal(t, "mongodb://localhost:27017", config.MongoURL)
+	assert.Equal(t, "mongodb://mongourl:27017", config.MongoURL)
 	assert.Equal(t, "fluentd:24224", config.FluentUrl)
 	assert.Equal(t, 8080, config.Port)
 	assert.Equal(t, "mysecret", config.JWTSecret)
